6819d02d854a1437af9d1255baeff138e6e3b282
solesen1992 <saramundal@live.dk>
2024-07-12 14:30:58 +0200

    Changes made to BMI calculator

--- BMI_calculator_WPF/MainWindow.xaml.cs
+++ BMI_calculator_WPF/MainWindow.xaml.cs
@@ -20,5 +20,67 @@ namespace BMI_calculator_WPF
         {
             InitializeComponent();
         }
+
+        private void btn_resultat(object sender, RoutedEventArgs e)
+        {
+
+            double hoejde = 0;
+            double vaegt = 0;
+            double BMI = 0;
+
+            try
+            {
+                //Konverterer til en double. Tager tal fra tbHoejde og tbVaegt, som er tekst og smider over i variabler som en double.
+                hoejde = Convert.ToDouble(tbHoejde.Text);
+                vaegt = Convert.ToDouble(tbVaegt.Text);
+                BMI = vaegt / (hoejde * hoejde);
+            }
+            catch (Exception exc)
+            {
+                //Hvis der opstår en fejl, så vises denne besked. Fx ved andet end tal.
+                MessageBox.Show(exc.Message, "How! Der er opstået en fejl...");
+            }
+            if (tbKoen.Text == "Mand")
+            {
+                if (BMI < 20)
+                {
+                    MessageBox.Show("Du er en undervægtig mand");
+                }
+                else if (BMI >= 20.0 && BMI <= 25.0)
+                {
+                    MessageBox.Show("Du er en normalvægtig mand");
+                }
+                else if (BMI >= 25 && BMI <= 30)
+                {
+                    MessageBox.Show("Du er en overvægtig mand");
+                }
+                else if (BMI > 30)
+                {
+                    MessageBox.Show("Du er en stærkt overvægtig mand");
+                }
+            }
+            else if (tbKoen.Text != "Mand")
+            {
+                if (BMI < 18.6)
+                {
+                    MessageBox.Show("Du er en slank kvinde");
+                }
+                else if (BMI >= 18.6 && BMI <= 23.8)
+                {
+                    MessageBox.Show("Du er en slank kvinde");
+                }
+                else if (BMI >= 23.8 && BMI <= 28.6)
+                {
+                    MessageBox.Show("Du er en slank kvinde");
+                }
+                else if (BMI > 28.6)
+                {
+                    MessageBox.Show("Du er en slank kvinde");
+                }
+            }
+
+        }
+
     }
+
 }
